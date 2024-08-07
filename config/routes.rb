67921b9bafdc905fb9f71e2fb044f3ae3eeb4e86
meo2326cc <cccmsp1@gmail.com>
2024-08-07 17:48:27 +0800

    Adjusted routes to make tasks the root path.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root to:"homepage#index"
+  #root to:"homepage#index"
   
-  resources :tasks
+  resources :tasks , path: '/'
   # Defines the root path route ("/")
   # root "posts#index"
 end
