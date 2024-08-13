a6b35b81ea4f64addf335131aeaefefb93e6a5ed
meo2326cc <cccmsp1@gmail.com>
2024-08-13 17:32:17 +0800

    Add form validation for required fields
    - Implemented model validations to ensure fields are not empty
    - Updated migration to reflect changes in model validations
    - Displayed error messages in the view for better user feedback
    - Added model tests to verify validation functionality

--- spec/factories/factory_data.rb
+++ spec/factories/factory_data.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :task do
     task_name { Faker::Lorem.word }
     description  { Faker::Lorem.sentence }
+
+    factory :empty_task do
+      task_name { "" }
+      description  { "" }
+    end
   end
 end
